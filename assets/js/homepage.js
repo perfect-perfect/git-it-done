3b6a4c2625152b11bd8ae49845fd760fdd2eb52b
perfect perfect <gilberto.j.covarrubias@gmail.com>
2022-01-08 00:19:01 -0500

    middle of 6.2.5

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,8 @@
+// creates the dom element at the form element on the HTML
+var userFormEl = document.querySelector("#user-form");
+
+// this creates a dom element at the text input where you search by user's name
+var nameInputEl = document.querySelector("#username");
 
 // note how we've added a parameter to getUserRepos's and then inserted the parameter into the GitHub API URL. We then use the newly formatted url in the subsequent fetch request
 var getUserRepos = function(user) {
@@ -8,12 +13,41 @@ var getUserRepos = function(user) {
     // fetch() is a web API that allows browsers to communicate with server-side API. It gets us JSON data.
     // then() is a callback function that can tha that can capture data from the API. It transforms it into something we can use
     fetch(apiURL).then(function(response) {
+
         // .json is a method that formats responses as JSON.
         // .json retuns another promise, hence the extra then(), whose callback function captures the actual data. This nested then() will make more sence as you get used to working with APIs
         response.json().then(function(data) {
-            console.log(data);
+
+            displayRepos(data, user);
+
+            // console.log(data); ---- saving this console.log() because i am curious about the use of data as an argument
         });
     });
 };
 
-getUserRepos("perfect-perfect");
+var formSubmitHandler = function(event) {
+    // weird notes on this. it stops browser from performing the default action the wants it to do. in this case, it prevents the browser from sending the form's input data to a URL (as we'll handle what happens with the from data ourselves in JavaScript)
+    event.preventDefault();
+    
+    // get value via the nameInputEl DOM variable .value and store the value in a new variable, username
+    var username = nameInputEl.value.trim();
+
+    //  if we have a username we pass that username as an argument to getUserRepos()
+    if (username) {
+        getUserRepos(username);
+        // i believe this clears the value field in the input text element.
+        nameInputEl.value = "";
+    }
+    else {
+        alert("Please enter a GitHub username");
+    }
+};
+
+var displayRepos = function(repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
+}
+
+// getUserRepos();
+
+userFormEl.addEventListener("submit", formSubmitHandler);
